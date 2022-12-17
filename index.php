4e58ae6251949885a77bed272f139d6d6b2ca60b
Sofyan Baharudin <sofyanbaharudin234@gmail.com>
2022-12-17 16:15:20 +0700

    kak_alfan

--- index.php
+++ index.php
@@ -1,19 +1,54 @@
 <?php
+// 1. Buat koneksi dengan MySQL
+$con = mysqli_connect("localhost","root","","fakultas");
 
-    echo "Hello World!<br>";
+// 2. Cek koneksi dengan MySQL
+if(mysqli_connect_error()){
+    echo "Koneksi gagal". mysqli_connect_error();
+} else{
+    echo "Koneksi berhasil";
+}
 
-    $nama = "Sofyan Baharudin";
-    $umur = 21;
+// 3. Membaca data dari tabel MySQL
+$query = "SELECT * FROM mahasiswa";
 
-    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+// 4. Tampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con,$query);
+$mahasiswa = [];
+if ($result){
+    // tampilkan data satu per satu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+}
 
-    $namaPonakan = "Ebra";
-    $umurPonakan = 2;
-
-    echo "Nama ponakan saya <strong>$namaPonakan</strong>, ponakan saya berusia $umurPonakan tahun.<br>";
-
-    $selisihUmur = $umur - $umurPonakan;
-    echo "Selisih usia saya dan ponakan saya adalah $selisihUmur tahun.";
-
+// 5. Tutup koneksi MySQL
+mysqli_close($con);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value): ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
